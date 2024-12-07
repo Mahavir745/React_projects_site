ec2f8e7430c0f99471df54bb51d5d0c486d43e46
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-07 15:23:51 +0530

    checking 1

--- src/component/ProjectsDetails.jsx
+++ src/component/ProjectsDetails.jsx
@@ -2,7 +2,7 @@ export let ProjectsData = [
   {
     title: "Todo List",
     details: "I built a to-do list app using React. It lets users add, edit, and delete tasks easily. The app is simple to use and helps me practice React basics, like managing state and creating components. It’s a great project to learn how to make interactive web apps.",
-    project_img: "src/assets/assest_02.png",
+    project_img: "./src/assets/assest_02.png",
     live_link: "https://todolist-phi-snowy.vercel.app/ ",
     source_link: "https://github.com/Mahavir745/todolist"
   },
