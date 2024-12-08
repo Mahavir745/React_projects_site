04784e6f68ac74336551b531e858f78a00024e69
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-08 11:48:53 +0530

    update project detailing

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({onButtonClick,darkmode,projectsLimts}) => {
   return (
     <div className='bg-gray-900 w-full h-[180px] flex items-center p-2 justify-around'>
       <div className='sm:w-[400px] md:w-[600px] rounded p-2'>
-        <h1 className='md:text-6xl sm:text-5xl font-extrabold text-red-500'>React <span className='text-yellow-400 font-light md:text-8xl sm:text-6xl'>{projectsLimts}</span> Projects</h1>
+        <h1 className='md:text-6xl sm:text-5xl text-4xl font-extrabold text-red-500'>React <span className='text-yellow-400 font-light md:text-8xl sm:text-6xl'>{projectsLimts}</span> Projects</h1>
         <p className='text-white font-extrabold text-right mt-2'>Managed by Mveer745</p>
       </div>
       <button className='text-white' onClick={onButtonClick}>{darkmode? <Sun/>: <Moon/>}</button>
