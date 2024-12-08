407ee78570dbb1876229267136864e7d80d2e233
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-08 07:25:29 +0530

    checking img

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -8,7 +8,7 @@ const Project = ({project}) => {
         <details className='text-center text-blue-800 font-semibold text-[14px] cursor-pointer '>
           <p className=' h-[100px] text-[13px] overflowContainer p-4  text-gray-700'>{project.details}</p>
         </details>
-        <img src={`${project.project_img}`} className='h-[200px] rounded-md' alt="" />
+        <img src="./src/assets/assest_01.jpg" className='h-[200px] rounded-md' alt="" />
         <div className='text-white flex justify-center items-center gap-6 bg-gray-900 p-5 h-[100px]' >
         <a href={`${project.live_link}`} target='_blank'><button className='bg-teal-700 pl-8 pr-8 pt-2 pb-2 rounded'>Live</button></a>
         <a href={`${project.source_link}` } target='_blank'><button className='bg-blue-700 pl-8 pr-8 pt-2 pb-2 rounded'>Source Code</button></a>
