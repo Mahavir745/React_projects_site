b4306b2750e8c8cb5535c8c07f7df65334ce0eda
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-09 09:31:57 +0530

    layout has been adjusted successfully

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -5,8 +5,8 @@ const Project = ({project,darkMode}) => {
     <div className={`w-[360px] sm:w-[400px] h-[500px] border rounded-xl bg-gray-100 ${darkMode && "bg-gray-900"}`}>
       <div className='p-2 flex flex-col justify-center gap-2'>
         <h1 className={`text-2xl text-center text-teal-900 font-semibold ${darkMode && "text-white"}`}>{project.title} </h1>
-        <details className='text-center text-blue-800 font-semibold text-[14px] cursor-pointer h-[140px]'>
-          <p className={`text-[13px] overflowContainer p-4 ${darkMode?"text-white":"text-teal-950"}`}>{project.details}</p>
+        <details className='text-center text-blue-800 font-semibold text-[14px] cursor-pointer h-[140px] overflowContainer'>
+          <p className={`text-[13px] p-4 text-left ${darkMode?"text-white":"text-teal-950"}`}>{project.details}</p>
         </details>
         <iframe src={`${project.project_img}`} width="320" height="180" allow="autoplay" className='rounded-2xl w-[340px] sm:w-[380px]'></iframe>
         <div className='text-white flex justify-center items-center gap-6 bg-gray-900 p-5 h-[100px]' >
